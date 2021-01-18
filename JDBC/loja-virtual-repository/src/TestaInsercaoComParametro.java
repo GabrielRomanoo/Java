5d59106ec1316bf3669fbf9e5a7f67ea564f69c6
GabrielRomanoo <gabriel.romano.3194@hotmail.com>
2021-01-18 17:00:30 -0300

    try-with-resources

--- JDBC/loja-virtual-repository/src/TestaInsercaoComParametro.java
+++ JDBC/loja-virtual-repository/src/TestaInsercaoComParametro.java
@@ -4,60 +4,60 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import javax.management.RuntimeErrorException;
+/*
+ * Por que, ao utilizar o try-with-resources, não é mais necessário explicitar o close para fechar o statements (ResultSet, Connection, PreparedStatement)?
+ * Estendendo o AutoCloseable, o try-with-resources executa o método close sem que precise estar explícito.
+ */
 
 public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
-		
-		String nome = "Mouse'"; //erro do usuario
-		String descricao = "Mouse sem fio); delete from Produto;"; //possivel vulnerabilidade
+
+		String nome = "Mouse'"; // erro do usuario
+		String descricao = "Mouse sem fio); delete from Produto;"; // possivel vulnerabilidade
 
 		ConnectionFactory connectionFactory = new ConnectionFactory();
-		Connection connection = connectionFactory.recuperarConexao();
-		
-		connection.setAutoCommit(false); //tira a responsabilidade de commitar do jdbc
-		
-		try {
-			//com o preparedstatement, o jdbc faz a validação do comando sql, a fim de nao ter possiveis erros nos parametros passados	
-			
-			PreparedStatement stm = connection
-					.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
-							Statement.RETURN_GENERATED_KEYS);  //novo paramentro para retornar o id do produto inserido
-			
-			adicionar("SmartTV", "45 polegadas", stm);
-			adicionar("Radio", "Radio de bateria", stm);
-			
-			connection.commit();
-					
-			stm.close();
-		} catch(Exception e) {
-			e.printStackTrace();
-			System.out.println("ROLLBACK EXECUTADO");
-			connection.rollback();			
+		try (Connection connection = connectionFactory.recuperarConexao()) {
+
+			connection.setAutoCommit(false); // tira a responsabilidade de commitar do jdbc
+
+			// com o preparedstatement, o jdbc faz a validação do comando sql, a fim de nao
+			// ter possiveis erros nos parametros passados
+			try (PreparedStatement stm = connection.prepareStatement(
+					"INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
+					Statement.RETURN_GENERATED_KEYS)) { /* novo paramentro para retornar o id do produto inserido */
+
+				adicionar("SmartTV", "45 polegadas", stm);
+				adicionar("Radio", "Radio de bateria", stm);
+
+				connection.commit();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("ROLLBACK EXECUTADO");
+				connection.rollback();
+			}
 		}
-		connection.close();
 	}
 
-	private static void adicionar(String nome, String descricao, PreparedStatement stm)
-			throws SQLException {
+	private static void adicionar(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
 
 		/*
-		if (nome.equals("Radio")) {
-			throw new RuntimeException("Não foi possivel adicionar o produto");
-		} */
-		
-		stm.execute(); 
-		
-		ResultSet rst = stm.getGeneratedKeys();
-		
-		while (rst.next()) {
-			Integer id = rst.getInt(1);
-			System.out.println("O id criado foi: " + id);
-		}
+		 * if (nome.equals("Radio")) { throw new
+		 * RuntimeException("Não foi possivel adicionar o produto"); }
+		 */
 
-		rst.close();
+		stm.execute();
+
+		try (ResultSet rst = stm.getGeneratedKeys()) { /* com o try-with-resources, ele irá fechar automaticamente o rst, pois o rst extende uma classe chamada  AutoClose */
+
+			while (rst.next()) {
+				Integer id = rst.getInt(1);
+				System.out.println("O id criado foi: " + id);
+			}
+
+		}
 	}
 }
--- JDBC/loja-virtual-repository/src/TestaRemocao.java
+++ JDBC/loja-virtual-repository/src/TestaRemocao.java
@@ -5,19 +5,19 @@ import java.sql.SQLException;
 public class TestaRemocao {
 
 	public static void main(String[] args) throws SQLException {
-		
+
 		ConnectionFactory connectionFactory = new ConnectionFactory();
-		Connection connection = connectionFactory.recuperarConexao();
-		
-		PreparedStatement stm = connection.prepareStatement("DELETE FROM PRODUTO WHERE ID > ?");
-		stm.setInt(1, 2);
-		
-		stm.execute();
-		
-		Integer linhasModificadas = stm.getUpdateCount(); //retorna o numero de linhas alteradas
-		
-		System.out.println("Quantidade de linhas modificadas: " + linhasModificadas);
-		connection.close();
+		try (Connection connection = connectionFactory.recuperarConexao()) { // try-with-resources (try-com-recursos)
+	
+			PreparedStatement stm = connection.prepareStatement("DELETE FROM PRODUTO WHERE ID > ?");
+			stm.setInt(1, 2);
+
+			stm.execute();
+
+			Integer linhasModificadas = stm.getUpdateCount(); // retorna o	 numero de linhas alteradas
+
+			System.out.println("Quantidade de linhas modificadas: " + linhasModificadas);
+		}
 	}
 
 }
