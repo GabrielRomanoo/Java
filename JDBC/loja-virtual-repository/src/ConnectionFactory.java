fd1a76878a7bcd3746b5aafed3b91e481213f4f9
GabrielRomanoo <gabriel.romano.3194@hotmail.com>
2021-01-19 16:35:48 -0300

    pool de conexões

--- JDBC/loja-virtual-repository/src/ConnectionFactory.java
+++ JDBC/loja-virtual-repository/src/ConnectionFactory.java
@@ -2,11 +2,33 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionFactory {
+import javax.sql.DataSource;
 
-	public Connection recuperarConexao() throws SQLException {
-		return DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC",
-				"root", "root");
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+/* classe para isolar, encapsular a chamada do driver manager */
+public class ConnectionFactory {
+	
+	public DataSource dataSource;
+	
+	/* criando o pool de conexões e configurando */
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("root");
+		
+		this.dataSource = comboPooledDataSource;
 	}
+	
+	public Connection recuperarConexao() throws SQLException {
+		return this.dataSource.getConnection();
+}
+	
+
+//	public Connection recuperarConexao() throws SQLException {
+//		return DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC",
+//				"root", "root");
+//	}
 
 }
