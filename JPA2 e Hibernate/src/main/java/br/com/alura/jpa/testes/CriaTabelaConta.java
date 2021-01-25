31aa74022c5c6efb842a93f69b28a22502a56a21
GabrielRomanoo <gabriel.romano.3194@hotmail.com>
2021-01-25 16:41:35 -0300

    transaction

--- JPA2 e Hibernate/src/main/java/br/com/alura/jpa/testes/CriaTabelaConta.java
+++ JPA2 e Hibernate/src/main/java/br/com/alura/jpa/testes/CriaTabelaConta.java
@@ -4,10 +4,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class TesteCriaTabelas {
+public class CriaTabelaConta {
 	
 	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas"); // mudar o persistence-unit name no persistence.xml para "contas"
 		EntityManager createEntityManager = emf.createEntityManager();
 		emf.close();
 	}
