54de159553883ddbc086a715962f3d3d5121d58a
GabrielRomanoo <gabriel.romano.3194@hotmail.com>
2021-01-25 17:08:19 -0300

    comentarios

--- JPA2 e Hibernate/src/main/java/br/com/alura/jpa/testes/AlteraSaldoContaLeonardo.java
+++ JPA2 e Hibernate/src/main/java/br/com/alura/jpa/testes/AlteraSaldoContaLeonardo.java
@@ -14,11 +14,16 @@ public class AlteraSaldoContaLeonardo {
 		
 		Conta contaDoLeonardo = em.find(Conta.class, 1L); //1L seria o id do registro que quero pegar
 		
+		//contaDoLeonardo se encontra no estado managed, porque a jpa tem uma referencia dela no banco. por conta disso, quando fazemos uma alteração, nao precisamos informar para a jpa que houve essa alteração, porque ela ja tem uma referencia da conta e automaticamente sabe que teve uma alteração
+		//em um estado managed, quando dado o commit, a jpa verifica atributo por atributo para ver se teve alteração
+		
 		em.getTransaction().begin();
 		
 		contaDoLeonardo.setSaldo(20.0); //o hibernate atualizou o saldo do leonardo no banco de dados por debaixo dos panos
 		
 		em.getTransaction().commit();
+		
+		//o estado managed é alcançado quando damos um find, ou um persist
 
 	}
 
---
+++ JPA2 e Hibernate/src/main/java/br/com/alura/jpa/testes/CriaContaComSaldo2.java
@@ -0,0 +1,36 @@
+package br.com.alura.jpa.testes;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.alura.jpa.modelo.Conta;
+
+public class CriaContaComSaldo2 {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura"); //fabrica de entity manager
+		EntityManager em = emf.createEntityManager(); //criando entity manager
+		
+		Conta conta = new Conta();
+		conta.setTitular("Leonardo");
+		conta.setNumero(12345);
+		conta.setAgencia(54321);
+		conta.setSaldo(500.0);
+		
+		//na jpa, nao vem por padrao o autocommit, como no jdbc
+		
+		em.getTransaction().begin();
+		
+		em.persist(conta);
+		
+		//conta esta em estado managed, e a jpa setou automaticamente o id. CUIDADO: nem toda conta com id é necessariamente managed
+		
+		conta.setSaldo(1000.0);
+		
+		em.getTransaction().commit();
+		
+	}
+
+}
