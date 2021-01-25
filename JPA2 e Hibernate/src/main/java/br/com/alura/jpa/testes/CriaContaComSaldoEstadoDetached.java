5bb034ee05d69198099a1dbe058fd6bd3ba062b3
GabrielRomanoo <gabriel.romano.3194@hotmail.com>
2021-01-25 17:36:50 -0300

    detached e merge

--- JPA2 e Hibernate/src/main/java/br/com/alura/jpa/testes/CriaContaComSaldoEstadoDetached.java
+++ JPA2 e Hibernate/src/main/java/br/com/alura/jpa/testes/CriaContaComSaldoEstadoDetached.java
@@ -6,7 +6,7 @@ import javax.persistence.Persistence;
 
 import br.com.alura.jpa.modelo.Conta;
 
-public class CriaContaComSaldo2 {
+public class CriaContaComSaldoEstadoDetached {
 
 	public static void main(String[] args) {
 		
@@ -14,22 +14,25 @@ public class CriaContaComSaldo2 {
 		EntityManager em = emf.createEntityManager(); //criando entity manager
 		
 		Conta conta = new Conta();
-		conta.setTitular("Leonardo");
+		conta.setTitular("Reinaldo");
 		conta.setNumero(12345);
 		conta.setAgencia(54321);
-		conta.setSaldo(500.0);
-		
-		//na jpa, nao vem por padrao o autocommit, como no jdbc
+		conta.setSaldo(100.0);
 		
 		em.getTransaction().begin();
 		
 		em.persist(conta);
 		
-		//conta esta em estado managed, e a jpa setou automaticamente o id. CUIDADO: nem toda conta com id é necessariamente managed
-		
-		conta.setSaldo(1000.0);
-		
 		em.getTransaction().commit();
+		em.close();
+		
+		// agora a conta virou para estado detached, pois o entity manager foi fechado
+		// no estado detached, você nao consegue fazer a sincronização automatica com o banco. para fazer isso, é preciso transformar o detached em managed, fazendo um merge
+		
+		conta.setSaldo(500.0);
+		System.out.println("id -> " + conta.getId());
+		
+		// mesmo tendo id, a conta deixou de ser managed para ser detached
 		
 	}
 
