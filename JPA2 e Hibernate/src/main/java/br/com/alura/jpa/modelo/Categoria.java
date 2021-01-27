110498b9c38d8ea74fdfea79af24cfe283c3b16e
GabrielRomanoo <gabriel.romano.3194@hotmail.com>
2021-01-27 17:10:00 -0300

    categoria

---
+++ JPA2 e Hibernate/src/main/java/br/com/alura/jpa/modelo/Categoria.java
@@ -0,0 +1,28 @@
+package br.com.alura.jpa.modelo;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
