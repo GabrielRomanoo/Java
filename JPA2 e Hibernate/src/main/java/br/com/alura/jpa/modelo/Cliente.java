ba7e28a7ebf511892e2afa168780970c63bd98fd
GabrielRomanoo <gabriel.romano.3194@hotmail.com>
2021-01-29 15:54:29 -0300

    cliente

--- JPA2 e Hibernate/src/main/java/br/com/alura/jpa/modelo/Cliente.java
+++ JPA2 e Hibernate/src/main/java/br/com/alura/jpa/modelo/Cliente.java
@@ -1,15 +1,19 @@
 package br.com.alura.jpa.modelo;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-public class Categoria {
-
+@Entity
+public class Cliente {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
+	private String profissao;
+	private String endereco;
 	
 	public Long getId() {
 		return id;
@@ -22,5 +26,17 @@ public class Categoria {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	public String getProfissao() {
+		return profissao;
+	}
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
 	}
 }
