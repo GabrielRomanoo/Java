8889e3e787acced52ec4f88388471d9754ecc535
GabrielRomanoo <gabriel.romano.3194@hotmail.com>
2021-02-06 19:05:49 -0300

    repository spring
    implementacao da interface repository

--- spring-data/src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ spring-data/src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -1,13 +1,31 @@
 package br.com.alura.spring.data;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringDataApplication {
+import br.com.alura.spring.data.orm.Cargo;
+import br.com.alura.spring.data.repository.CargoRepository;
 
+@SpringBootApplication
+public class SpringDataApplication implements CommandLineRunner {
+
+	private final CargoRepository repository;
+	
+	public SpringDataApplication(CargoRepository repository) {
+		this.repository = repository;
+	}
+	
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDataApplication.class, args);
+		SpringApplication.run(SpringDataApplication.class, args); //essa linha faz com que o framework do Spring seja inicializado junto a nossa aplicação
+	}
+
+	@Override //apos o fim do metodo main, esse metodo sera rodado
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
+		
+		repository.save(cargo);
 	}
 
 }
